5a5ffd951786bbb812c4114d4441a0bc70cf769b
Lin Dong <ldong@gatech.edu>
2014-10-05 17:33:06 -0400

    Add SellController and BuyController

---
+++ GTBarterSystem2/GTBarterSystem2/BuyController.swift
@@ -0,0 +1,23 @@
+//
+//  BuyController.swift
+//  GTBarterSystem2
+//
+//  Created by Lin Dong on 10/5/14.
+//  Copyright (c) 2014 Lin Dong. All rights reserved.
+//
+
+import UIKit
+
+class BuyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ GTBarterSystem2/GTBarterSystem2/SellController.swift
@@ -0,0 +1,25 @@
+//
+//  SellController.swift
+//  GTBarterSystem2
+//
+//  Created by Lin Dong on 10/5/14.
+//  Copyright (c) 2014 Lin Dong. All rights reserved.
+//
+
+import UIKit
+
+class SellController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
