59549144040bac4342292a2c397873aedcc3c893
“Haytham <haysam121@gmail.com>
2014-10-15 18:04:52 -0400

    update for restful

--- GTBarterSystem2/GTBarterSystem2/ViewController.swift
+++ GTBarterSystem2/GTBarterSystem2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDelegate {
 
   @IBOutlet weak var usernameTextField: UITextField!
   @IBOutlet weak var readBtn: UIButton!
@@ -16,22 +16,81 @@ class ViewController: UIViewController {
   @IBOutlet weak var btn: UIButton!
   var myRootRef: Firebase!
   var users : [User] = [User]()
+  lazy var data = NSMutableData()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     connectToThanhsServer()
     // Do any additional setup after loading the view, typically from a nib.
-    setupFirebase()
+   searchFunction()
+    //startConnection()
+    // setupFirebase()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+    
+    func searchFunction() {
+        var url : NSURL = NSURL(string: "https://itunes.apple.com/search?term=&media=software")!
+        var request: NSURLRequest = NSURLRequest(URL:url)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+            
+            var newdata : NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            
+            var info : NSArray =  newdata.valueForKey("results") as NSArray
+            
+            var name: String? = info.valueForKey("trackName") as? String // Returns nil
+            println(name)//Returns nil
+            
+            
+            var name2 : NSString = info.valueForKey("trackName") as NSString //Crashes
+            println(name2) //Crashes
+            
+        });
+        
+        
+        task.resume()
+        println("Resumed")
+        
+    }
+    
+    
+    
+    func startConnection(){
+        
+        var url : String = "http://54.86.116.203:3000/items"
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        println("34534")
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler:{(data, response, error) in
+            var newdata : NSArray = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSArray
+            
+            var name: String? = newdata[0].valueForKey("name") as? NSString
+            println(name)
+        });
+        task.resume()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        self.data.appendData(data)
+    }
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        // throwing an error on the line below (can't figure out where the error message is)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        println(jsonResult)
+    }
   
     func connectToThanhsServer(){
         println("in 1")
-        let url = NSURL(string: "http://myrestservice")
+        let url = NSURL(string: "")
         let theRequest = NSURLRequest(URL: url!)
         
         NSURLConnection.sendAsynchronousRequest(theRequest, queue: nil, completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
