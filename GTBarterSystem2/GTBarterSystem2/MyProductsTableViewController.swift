d59ee0427e562023dcca7a97c7db69abc0eda34b
“Haytham <haysam121@gmail.com>
2014-11-27 20:05:23 -0500

    my products

--- GTBarterSystem2/GTBarterSystem2/MyProductsTableViewController.swift
+++ GTBarterSystem2/GTBarterSystem2/MyProductsTableViewController.swift
@@ -7,52 +7,53 @@
 //
 
 import UIKit
+import SwiftHTTP
+import JSONJoy
 
 class MyProductsTableViewController: UITableViewController {
-
-    var userId:AnyObject!
     
+    var userId:AnyObject?
+    var userProducts: [Product] = []
+    var cookie:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         println("user id \(self.userId)")
-
+        println(userProducts)
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
+        //    self.clearsSelectionOnViewWillAppear = false
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        return userProducts.count
     }
-
-    /*
-    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:CustomTableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as CustomTableViewCell
+        
+        var product : Product
+        // Check to see whether the normal table or search results table is being displayed and set the Candy object from the appropriate array
+        product = userProducts[indexPath.row]
+        // Configure the cell
+        //cell.textLabel!.text = product.title
+        
+        println(product.title)
+        //    println(cell)
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        cell.productTitle.text = product.title
+        cell.productId.text = product._id
+        
         return cell
     }
-    */
-
     
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -65,36 +66,39 @@ class MyProductsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            println("edittttinnggg")
+            var cell: CustomTableViewCell = tableView.cellForRowAtIndexPath(indexPath) as CustomTableViewCell
+            self.userProducts.removeAtIndex(indexPath.row)
+            self.deleteItemFromServer("http://54.86.116.203:3000/items/", itemID: cell.productId.text!, ret: { (result:Bool) -> (Void) in
+                println("buy button is:  \(result) ")
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    
+                }
+            })
+            
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
-
+    
+    func deleteItemFromServer(url: String, itemID:String, ret: (Bool) -> (Void)) -> Void{
+        var request = HTTPTask()
+        var finalUrl = url + itemID
+        println(finalUrl)
+        request.DELETE(finalUrl, parameters: nil, success: {(response: HTTPResponse) in
+            if response.responseObject != nil {
+                let data = response.responseObject as NSData
+                let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+                let items = Items(JSONDecoder(response.responseObject!))
+                
+            }
+            },failure: {(error: NSError) in
+                println(" error \(error)")
+        })
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
